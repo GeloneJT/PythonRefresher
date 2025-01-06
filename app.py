0f4ff2efdc670cee7b03eab70cf5efd01b027057
Jelani <eyeglass0324@gmail.com>
2025-01-06 10:26:07 -0500

    Condtional Statements

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-x = input("x: ")
-y = int(x) + 1
-print(f"x: {x} y: {y}")
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
