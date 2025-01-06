b2b07dfd613eff5879310f044fa0231cc0415929
Jelani <eyeglass0324@gmail.com>
2025-01-06 12:06:31 -0500

    Control Flow exercise

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-for x in range(5):
-    for y in range(3):
-        print(f"({x}, {y})")
+# Write a program to display all the even numbers between 1 to 10 and then print a statement showing the count of even numbers
+
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count += 1
+        print(number)
+else:
+    print(f"We have {count} even numbers ")
