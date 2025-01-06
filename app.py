d990e633b700f83093ec25a0ca691338099eef0b
Jelani <eyeglass0324@gmail.com>
2025-01-06 09:24:50 -0500

    Formatted Strings

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-students_count = 1000
-rating = 4.99
-is_published = False
-course_name = "Python Programming"
-print(students_count)
-
-
+first = "Jelani"
+last = "Thomas"
+full = f"{first} {last}"
+print(full)
