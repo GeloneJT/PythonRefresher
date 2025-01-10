d8036c17892ccf8515a25f345960ab55364551b4
Jelani <eyeglass0324@gmail.com>
2025-01-10 07:16:11 -0500

    udated naming convention

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-# Write a program to display all the even numbers between 1 to 10 and then print a statement showing the count of even numbers
+''' Write a program to display all the even numbers between 1 to 10
+ and then print a statement showing the COUNT of even numbers'''
 
-count = 0
+COUNT = 0
+
 for number in range(1, 10):
     if number % 2 == 0:
-        count += 1
+        COUNT += 1
         print(number)
-else:
-    print(f"We have {count} even numbers ")
+print(f"We have {COUNT} even numbers ")
