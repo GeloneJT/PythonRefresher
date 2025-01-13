974ea23d42808c930b1bd26a308896eaeccb2b1a
Jelani <eyeglass0324@gmail.com>
2025-01-13 10:32:26 -0500

    Add and remove items from a list syntax

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-'''Python Functions - FizzBuzz'''
+'''Python Functions - Lists'''
 
 
-def fizzbuzz(user_input):
-    ''''Fizzbuzz'''
-    if (user_input % 3 == 0) and (user_input % 5 == 0):
-        return "FizzBuzz"
-    if user_input % 3 == 0:
-        return "Fizz"
-    if user_input % 5 == 0:
-        return "Buzz"
-    return user_input
+letters_add = ["a", "b", "c"]
+# Add
+letters_add.append("d")  #  add to the end
+letters_add.insert(0, "-")  #  add to the beginning
+print(letters_add)
 
-print(fizzbuzz(7))
+
+letters_remove = ["a", "b", "c", "d", "e"]
+# Remove
+letters_remove.pop()  # remove from the last index
+letters_remove.remove("b")  #  removes the first occurence of the arg
+del letters_remove[0:1]  #  deletes the objects in the index range
+letters_remove.clear()  #  clears the list
+print(letters_remove)
