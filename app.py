5c612f74d320b3c7fd0cca44fb1afbbdd67c17db
Jelani <eyeglass0324@gmail.com>
2025-01-13 11:54:45 -0500

    Sets syntax

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-'''Python Functions - Queues'''
+'''Python Functions - Sets'''
 
-from collections import deque
+numbers = [1, 2, 3, 4]
+first = set(numbers)
+second = {1, 5}
 
-queue =  deque([])
-queue.append(1)
-queue.append(2)
-queue.append(3)
-queue.popleft()
-print(queue)
-if not queue:
-    print("empty")
+# returns a new set of all items that are in the first or second set  {1, 2, 3, 4, 5}
+print(first | second)
+# returns a new set of all the items that are both in the first and second set {1}
+print(first & second)
+# returns a new set of the difference of the items of the first and second set {2, 3, 4}
+print(first - second)
+#  returns a new set of items that are in the first or second set but not both {2, 3, 4, 5}
+print(first ^ second)
