ee9b5bd0c6e74f21a675c8b17dc13f690868c431
Jelani <eyeglass0324@gmail.com>
2025-01-13 11:08:18 -0500

    queue syntax

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-'''Python Functions - Lists'''
+'''Python Functions - Queues'''
 
+from collections import deque
 
-letters_add = ["a", "b", "c"]
-# Add
-letters_add.append("d")  #  add to the end
-letters_add.insert(0, "-")  #  add to the beginning
-print(letters_add)
-
-
-letters_remove = ["a", "b", "c", "d", "e"]
-# Remove
-letters_remove.pop()  # remove from the last index
-letters_remove.remove("b")  #  removes the first occurence of the arg
-del letters_remove[0:1]  #  deletes the objects in the index range
-letters_remove.clear()  #  clears the list
-print(letters_remove)
+queue =  deque([])
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.popleft()
+print(queue)
+if not queue:
+    print("empty")
