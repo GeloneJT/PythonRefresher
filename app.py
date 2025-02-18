3775f19ca92d2a2fc8b280ae0ee35d294794f946
Jelani <eyeglass0324@gmail.com>
2025-02-18 12:35:46 -0500

    Custom Containers

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
-'''Python Classes: Magic Methods'''
+'''Python Classes: Custom Containers'''
 
 
-class Point:
-    '''create custom class Point'''
+class TagCloud:
+    '''Initialize tags to an empty dict
+    __tags is to private'''
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self):
+        self.__tags = {}
 
-    def __add__(self, other):
-        '''Adds the points'''
-        return Point(self.x + other.x, self.y + other.y)
+    def add(self, tag):
+        '''Adds tag attribute to dict if it doesn't exist, otherwise it increments by 1'''
+        self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0) + 1
+
+    def _getitem__(self, tag):
+        '''Gets a count of the tag attribute'''
+        return self.__tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        '''Sets the tag count'''
+        self.__tags[tag.lower()] = count
+
+    def _len__(self):
+        return len(self.__tags)
+
+    def __iter__(self):
+        return iter(self.__tags)
 
 
-point = Point(10, 20)
-another = Point(1, 2)
-combined = point + another
-print(combined.x)
+cloud = TagCloud()
